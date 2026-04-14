ddd63bc82d090de6b0fc67ac3f6a7de7b912ca41
paulich07 <50242426+paulich07@users.noreply.github.com>
2026-04-14 16:43:18 +0200

    rotating cube with delta time

--- src/test.ts
+++ src/test.ts
@@ -1,19 +1,22 @@
 import * as THREE from 'three/webgpu';
+import { Inspector } from 'three/examples/jsm/inspector/Inspector.js';
 
 import './main.css';
 
 const main = async () => {
+    /* Creazione elemento "app" */
     const container = document.createElement('app');
 
     const renderer = new THREE.WebGPURenderer({ });
     await renderer.init();
+
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0, 5, 5);
     camera.lookAt(0, 0, 0);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const geometry = new THREE.BoxGeometry(1, 1, 2);
     /* Mesh = geometria + materiale */
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
@@ -27,9 +30,33 @@ const main = async () => {
     directionalLight.position.set(1, 1, 1);
     scene.add(directionalLight);
 
+    /* Axes helper */
+    const axesHelper = new THREE.AxesHelper(5);
+    axesHelper.position.set(0, 0, 0);
+    scene.add(axesHelper);
+
+    /* Grid helper */
+    const gridHelper = new THREE.GridHelper(10, 10);
+    scene.add(gridHelper);
+
+    /* Inspector (ANCORA SPERIMENTALE) va inizializzato dopo che la scena e il renderer sono pronti */
+    // const inspector = new Inspector();
+    // container.appendChild(inspector.domElement);
+
+    /* Per misurare il tempo di rendering */
+    const timer = new THREE.Timer();
+
+    /* il browser e' cappato a 60fps */
     renderer.setAnimationLoop(() => {
+        // inspector.begin();
+        timer.update();
+        const delta = timer.getDelta();
+        cube.rotation.y += 1 * delta; // delta normalizza la velocita'
+        // cube.rotation.y += 0.1;
         renderer.render(scene, camera);
+        // inspector.finish();
     });
+
 
     const onResize = () => {
         const width = window.innerWidth;
@@ -43,7 +70,9 @@ const main = async () => {
 
     window.addEventListener('resize', onResize);
     onResize();
+    /* Aggiungo "app" al DOM */
     document.body.appendChild(container);
+    /* Aggiungo renderer.domElement (CANVAS) al DOM */
     container.appendChild(renderer.domElement);
 }
 main();
